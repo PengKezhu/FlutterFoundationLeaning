1f59a3c0ff812a14d776a828fb83530caade1da7
kezhu <pengkezhu@7mtt.cn>
2019-02-19 15:23:07 +0800

    Shopping list example

--- flutter_foundataion_demo/lib/main.dart
+++ flutter_foundataion_demo/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'dart:developer';
 
 void main() {
   runApp(MyApp());
@@ -26,7 +25,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyScafford(),
+      home: new ShoppingList(
+        products: <Product>[
+          new Product(name: 'fish'),
+          new Product(name: 'milk'),
+          new Product(name: 'bread'),
+        ],
+      ),
     );
   }
 }
@@ -39,14 +44,13 @@ class MyScafford extends StatelessWidget {
         children: <Widget>[
           new MyAppBar(
             title: new Container(
-              color: Colors.yellow,
                 child: new Text('MyAppBar',
                     textAlign: TextAlign.center,
                     style: new TextStyle(color: Colors.white))),
           ),
           new Expanded(
             child: new Center(
-              child: new Text('Hello World!'),
+              child: new MyButton(),
             ),
           )
         ],
@@ -54,6 +58,36 @@ class MyScafford extends StatelessWidget {
     );
   }
 }
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: () {
+        print('MyButton tapped');
+      },
+      child: new Container(
+          height: 36.0,
+          padding: new EdgeInsets.all(8.0),
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          decoration: new BoxDecoration(
+              borderRadius: new BorderRadius.circular(5.0),
+              color: Colors.lightGreen),
+          child: new Container(
+              color: Colors.red,
+              child: new Center(
+                child: new Text('BUTTON'),
+              ))),
+    );
+  }
+}
+
+/**
+ *  new Center(
+            child: new Text('BUTTON'),
+          )
+ * 
+ */
 
 class MyAppBar extends StatelessWidget {
   MyAppBar({this.title});
@@ -86,6 +120,89 @@ class MyAppBar extends StatelessWidget {
               ),
             )
           ],
+        ));
+  }
+}
+
+class Product {
+  const Product({this.name});
+  final String name;
+}
+
+typedef void CartChangedCallback(Product product, bool inCart);
+
+class ShopItem extends StatelessWidget {
+  ShopItem({Product product, this.inCart, this.onCartChanged})
+      : product = product,
+        super(key: new ObjectKey(product));
+
+  final CartChangedCallback onCartChanged;
+  final bool inCart;
+  final Product product;
+
+  TextStyle _getTextStyle() {
+    if (!inCart) return null;
+    return new TextStyle(
+        color: Colors.black54, decoration: TextDecoration.lineThrough);
+  }
+
+  Color _getBackgroundColor(BuildContext context) {
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(
+      onTap: () {
+        onCartChanged(product, !inCart);
+      },
+      leading: new CircleAvatar(
+        backgroundColor: _getBackgroundColor(context),
+        child: new Text(product.name[0]),
+      ),
+      title: new Text(
+        product.name,
+        style: _getTextStyle(),
+      ),
+    );
+  }
+}
+
+class ShoppingList extends StatefulWidget {
+  ShoppingList({Key key, this.products}) : super(key: key);
+
+  final List<Product> products;
+
+  @override
+  State<StatefulWidget> createState() {
+    return new ShoppingListState();
+  }
+}
+
+class ShoppingListState extends State<ShoppingList> {
+  Set<Product> _shoppingCart = new Set<Product>();
+
+  void _onCartChanged(Product product, bool inCart) {
+    setState(() {
+      if (inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(title: new Text('ShopCart')),
+        body: new ListView(
+          children: widget.products.map((Product product) {
+            return new ShopItem(
+                product: product,
+                inCart: _shoppingCart.contains(product),
+                onCartChanged: _onCartChanged);
+          }).toList(),
         ));
   }
 }
